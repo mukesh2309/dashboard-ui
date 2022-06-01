9ae5e4fe01fc315020cceccb9497526f86a9e4e0
mukesh2309 <shelkemukesh6@gmail.com>
2022-06-01 14:37:44 +0530

    added updates

--- src/Components/Activity.jsx
+++ src/Components/Activity.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import style from "./Activity.module.css";
-import julie from "../Images/julie.png";
 
 function Activity({ name, avatar, target, action, created_at }) {
   return (
     <div className={style.activity_info}>
-      <img src={avatar} width="15%" style={{ borderRadius: "50%" }} />
+      <img src={avatar} width="40rem" style={{ borderRadius: "50%" }} />
       <div>
         <p style={{ fontSize: "1.2rem" }}>
           {name}
-          <span style={{ padding: "0 .4rem" }}>{action}</span>
+          {action}
           {target}
         </p>
         <span style={{ color: "#565656" }}>{created_at}</span>
